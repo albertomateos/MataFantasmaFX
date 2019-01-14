a79fbf888ca26f89544deca90cd8204c0a09b6bd
PC07 <PC07@DESKTOP-U7KJ0D0>
2019-01-14 12:58:02 +0100

    tarea3

--- src/com/albertomateosatienza/MataFantasmaFX.java
+++ src/com/albertomateosatienza/MataFantasmaFX.java
@@ -5,11 +5,16 @@
  */
 package com.albertomateosatienza;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.shape.Circle;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
+import javafx.application.Application; 
+import javafx.scene.Group;
+import javafx.scene.Scene; 
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color; 
+import javafx.scene.shape.Arc; 
+import javafx.scene.shape.ArcType;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.StrokeType;
+import javafx.stage.Stage; 
 
 
 
@@ -17,22 +22,31 @@ import javafx.stage.Stage;
  
     public class MataFantasmaFX extends Application {
     
-    @Override
-    public void start(Stage primaryStage) {   
-    StackPane root = new StackPane();
-    Scene scene = new Scene(root, 600, 400);
-    primaryStage.setTitle("PONGFX");
-    primaryStage.setScene(scene);
-    primaryStage.show();
-    
-    Circle circleBall = new Circle(10,30,7);
-    root.getChildren().add(circleBall);
-    }
+    @Override public void start(Stage primaryStage) {
+        Pane root = new Pane();
+        Scene scene = new Scene(root, 600,400,Color.BLUE);
+        primaryStage.setTitle("MataFantasma");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        
+        Arc arc = new Arc(90, 50, 25, 25, 0, 180); 
+        arc.setType(ArcType.OPEN); 
+        arc.setStrokeWidth(50); 
+        arc.setStroke(Color.RED); 
+        arc.setStrokeType(StrokeType.INSIDE); 
+        arc.setFill(null); 
+        root.getChildren().add(arc);
+        
+        Rectangle pata1 = new Rectangle();
+        pata1.setX(100);
+        pata1.setY(100);
+        pata1.setWidth(50);
+        pata1.setHeight(20);
+        pata1.setFill(Color.BLACK);
+    } 
     /**
-     * @param args the command line arguments
+     * 
+     * @param args the commant line arguments
      */
-    public static void main(String[] args) {
-        launch(args);
-    }
-    
-}
+    public static void main(String[] args) { launch(args); } 
+} 
---
+++ src/com/albertomateosatienza/stage.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.albertomateosatienza;
+
+import javafx.scene.Scene;
+
+/**
+ *
+ * @author PC07
+ */
+class stage {
+
+    static void show() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void setScene(Scene scene) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
