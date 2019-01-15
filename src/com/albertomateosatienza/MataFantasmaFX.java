139f502cfaf6ba0c16331d4ee01ff0aa2661a254
PC07 <PC07@DESKTOP-U7KJ0D0>
2019-01-15 10:46:48 +0100

    tarea4

--- src/com/albertomateosatienza/MataFantasmaFX.java
+++ src/com/albertomateosatienza/MataFantasmaFX.java
@@ -5,8 +5,11 @@
  */
 package com.albertomateosatienza;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application; 
+import javafx.collections.ObservableList;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene; 
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color; 
@@ -21,28 +24,147 @@ import javafx.stage.Stage;
  
  
     public class MataFantasmaFX extends Application {
+        int medusaCenterX = 90;
+        int disparador = 85;
+        int lado1 = 70;
+        int lado2 = 85;
+        int lado3 = 100; 
+        int velocidad = 3;
+        
+                
     
     @Override public void start(Stage primaryStage) {
         Pane root = new Pane();
-        Scene scene = new Scene(root, 600,400,Color.BLUE);
+        Scene scene = new Scene(root, 600,400,Color.BLACK);
         primaryStage.setTitle("MataFantasma");
         primaryStage.setScene(scene);
         primaryStage.show();
         
-        Arc arc = new Arc(90, 50, 25, 25, 0, 180); 
+        Arc arc = new Arc(medusaCenterX,50, 25, 25, 0, 180); 
         arc.setType(ArcType.OPEN); 
         arc.setStrokeWidth(50); 
         arc.setStroke(Color.RED); 
         arc.setStrokeType(StrokeType.INSIDE); 
         arc.setFill(null); 
-        root.getChildren().add(arc);
+        
         
         Rectangle pata1 = new Rectangle();
-        pata1.setX(100);
-        pata1.setY(100);
-        pata1.setWidth(50);
-        pata1.setHeight(20);
-        pata1.setFill(Color.BLACK);
+        pata1.setX(lado1);
+        pata1.setY(50);
+        pata1.setWidth(10);
+        pata1.setHeight(40);
+        pata1.setFill(Color.YELLOW);
+        
+        
+        Rectangle pata2 = new Rectangle();
+        pata2.setX(lado2);
+        pata2.setY(50);
+        pata2.setWidth(10);
+        pata2.setHeight(40);
+        pata2.setFill(Color.YELLOW);
+       
+        
+        Rectangle pata3 = new Rectangle();
+        pata3.setX(lado3);
+        pata3.setY(50);
+        pata3.setWidth(10);
+        pata3.setHeight(40);
+        pata3.setFill(Color.YELLOW);
+        
+        
+        Rectangle boca = new Rectangle();
+        boca.setX(disparador);
+        boca.setY(20);
+        boca.setWidth(10);
+        boca.setHeight(10);
+        boca.setFill(Color.RED);
+        
+        
+        Group groupPerson = new Group();
+        groupPerson.getChildren().add(arc);
+        groupPerson.getChildren().add(pata1);
+        groupPerson.getChildren().add(pata2);
+        groupPerson.getChildren().add(pata3);
+        groupPerson.getChildren().add(boca);
+        groupPerson.setLayoutX(medusaCenterX);
+        groupPerson.setLayoutY(50);
+        
+        
+        root.getChildren().add(groupPerson);
+        
+        AnimationTimer animationmedusa;
+            animationmedusa = new AnimationTimer(){
+                @Override
+                public void  handle(long now){
+                    medusaCenterX++;
+                    arc.setCenterX(medusaCenterX);
+                    medusaCenterX += velocidad;
+                    if (medusaCenterX >= 600){
+                        velocidad = -3;
+                    }
+                    if (medusaCenterX <= 0){
+                        velocidad = 3;
+                    }
+                    
+                    
+                    disparador++;
+                    boca.setX(disparador);
+                    disparador += velocidad;
+                    if(disparador >= 600){
+                        velocidad = -3;
+                    }
+                    if(disparador <= 0){
+                        velocidad = 3;
+                    }
+                    
+                    lado1++;
+                    pata1.setX(lado1);
+                    lado1 += velocidad;
+                    if(lado1 >= 600){
+                        velocidad = -3;
+                    }
+                    if(lado1 <= 0){
+                        velocidad = 3;
+                    }
+                    
+                    lado2++;
+                    pata2.setX(lado2);
+                    lado2 += velocidad;
+                    if(lado2 >= 600){
+                        velocidad = -3;
+                    }
+                    if(lado2 <= 0){
+                        velocidad = 3;
+                    }
+                    
+                    
+                    lado3++;
+                    pata3.setX(lado3);
+                    lado3 += velocidad;
+                    if(lado3 >= 600){
+                        velocidad = -3;
+                    }
+                    if(lado3 <= 0){
+                        velocidad = 3;
+                    }
+                    
+                    
+                    
+                   System.out.println(medusaCenterX);
+                   System.out.println(disparador);
+                   System.out.println(lado1);
+                   System.out.println(lado2);
+                   System.out.println(lado3);
+                    
+                    
+                    
+                };
+            };
+               
+         animationmedusa.start();
+          
+        
+        
     } 
     /**
      * 
