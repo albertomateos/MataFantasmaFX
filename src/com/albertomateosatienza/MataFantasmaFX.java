5ba07476e52bc46a8fd8ad9adfcb563846674dc3
PC07 <PC07@DESKTOP-U7KJ0D0>
2019-01-16 10:37:44 +0100

    tarea5

--- src/com/albertomateosatienza/MataFantasmaFX.java
+++ src/com/albertomateosatienza/MataFantasmaFX.java
@@ -7,9 +7,7 @@ package com.albertomateosatienza;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application; 
-import javafx.collections.ObservableList;
 import javafx.scene.Group;
-import javafx.scene.Node;
 import javafx.scene.Scene; 
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color; 
@@ -29,7 +27,9 @@ import javafx.stage.Stage;
         int lado1 = 70;
         int lado2 = 85;
         int lado3 = 100; 
-        int velocidad = 3;
+        int velocidadx = 2;
+        int medusaX = 0;
+        
         
                 
     
@@ -86,8 +86,8 @@ import javafx.stage.Stage;
         groupPerson.getChildren().add(pata2);
         groupPerson.getChildren().add(pata3);
         groupPerson.getChildren().add(boca);
-        groupPerson.setLayoutX(medusaCenterX);
-        groupPerson.setLayoutY(50);
+        groupPerson.setLayoutX(0);
+        groupPerson.setLayoutY(300);
         
         
         root.getChildren().add(groupPerson);
@@ -96,65 +96,18 @@ import javafx.stage.Stage;
             animationmedusa = new AnimationTimer(){
                 @Override
                 public void  handle(long now){
-                    medusaCenterX++;
-                    arc.setCenterX(medusaCenterX);
-                    medusaCenterX += velocidad;
-                    if (medusaCenterX >= 600){
-                        velocidad = -3;
+                    groupPerson.setLayoutX(medusaX);
+                    medusaX+= velocidadx;
+                    
+                    if (medusaX >=475){
+                        velocidadx = -2;
+                        
                     }
-                    if (medusaCenterX <= 0){
-                        velocidad = 3;
+                    if (medusaX <=-50){
+                        velocidadx = 2;
+                        
                     }
                     
-                    
-                    disparador++;
-                    boca.setX(disparador);
-                    disparador += velocidad;
-                    if(disparador >= 600){
-                        velocidad = -3;
-                    }
-                    if(disparador <= 0){
-                        velocidad = 3;
-                    }
-                    
-                    lado1++;
-                    pata1.setX(lado1);
-                    lado1 += velocidad;
-                    if(lado1 >= 600){
-                        velocidad = -3;
-                    }
-                    if(lado1 <= 0){
-                        velocidad = 3;
-                    }
-                    
-                    lado2++;
-                    pata2.setX(lado2);
-                    lado2 += velocidad;
-                    if(lado2 >= 600){
-                        velocidad = -3;
-                    }
-                    if(lado2 <= 0){
-                        velocidad = 3;
-                    }
-                    
-                    
-                    lado3++;
-                    pata3.setX(lado3);
-                    lado3 += velocidad;
-                    if(lado3 >= 600){
-                        velocidad = -3;
-                    }
-                    if(lado3 <= 0){
-                        velocidad = 3;
-                    }
-                    
-                    
-                    
-                   System.out.println(medusaCenterX);
-                   System.out.println(disparador);
-                   System.out.println(lado1);
-                   System.out.println(lado2);
-                   System.out.println(lado3);
                     
                     
                     
